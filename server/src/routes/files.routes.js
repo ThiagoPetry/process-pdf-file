9c8c9329539cf6a52ccd0d8a6bdf8befccc33044
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2023-10-08 01:21:08 -0300

    refactor: added cb to read pdf

--- server/src/routes/files.routes.js
+++ server/src/routes/files.routes.js
@@ -3,41 +3,36 @@ const request = require("request-promise");
 
 const router = express.Router();
 
+const readPdf = async (file, cb) => {
+  const options = {
+    method: "POST",
+    uri: "http://localhost:8081/read_pdf",
+    json: true,
+    formData: {
+      file: {
+        value: file.pdf.data,
+        options: {
+          filename: file.pdf.name,
+          contentType: file.pdf.mimetype,
+        },
+      },
+    },
+  };
+
+  const response = await request(options);
+  cb(response);
+};
+
 router.post("/read_pdf", async (req, res) => {
   try {
     const file = req.files;
 
-    const options = {
-      method: "POST",
-      uri: "http://localhost:8081/read_pdf",
-      json: true,
-      formData: {
-        file: {
-          value: file.pdf.data,
-          options: {
-            filename: file.pdf.name,
-            contentType: file.pdf.mimetype,
-          },
-        },
-      },
-    };
-
-    const response = await request(options);
-
-    return res.status(200).json({ ...response });
+    readPdf(file, (response) => {
+      return res.status(200).json({ ...response });
+    });
   } catch (error) {
-    let statusCode = 500;
-
-    if (error?.statusCode) {
-      statusCode = error.statusCode;
-    }
-
-    if (error?.error) {
-      error = error.error;
-    }
-
-    const response = error;
-    response.error = true;
+    const statusCode = error?.statusCode || 500;
+    const response = error?.error || error;
 
     return res.status(statusCode).json({ response });
   }
