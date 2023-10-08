94258f884714f6967bfb5d6b442e82e3844d7c11
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2023-10-08 19:51:32 -0300

    feat: added new route to update pdf

--- server/src/routes/files.routes.js
+++ server/src/routes/files.routes.js
@@ -38,4 +38,45 @@ router.post("/read_pdf", async (req, res) => {
   }
 });
 
+const updatePdf = async (file, text, cb) => {
+  const options = {
+    method: "POST",
+    uri: "http://localhost:8081/update_pdf",
+    json: true,
+    formData: {
+      file: {
+        value: file.pdf.data,
+        options: {
+          filename: file.pdf.name,
+          contentType: file.pdf.mimetype,
+        },
+      },
+      text: text,
+    },
+  };
+
+  await request(options).then((buffer) => {
+    const test = Buffer.from(buffer);
+    cb(test);
+  }).catch((err) => {
+    cb(err);
+  });
+};
+
+router.post("/update_pdf", async (req, res) => {
+  try {
+    const file = req.files;
+    const text = req.body.text;
+
+    updatePdf(file, text, (response) => {
+      return res.status(200).json({ response });
+    });
+  } catch (error) {
+    const statusCode = error?.statusCode || 500;
+    const response = error?.error || error;
+
+    return res.status(statusCode).json({ response });
+  }
+});
+
 module.exports = router;
